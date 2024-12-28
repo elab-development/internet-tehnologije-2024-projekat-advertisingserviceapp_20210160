b2526c5e515ca43dc38892368619dee7cbcc905d
Nikola Nestorovic <nestornikola002@gmail.com>
2024-12-28 18:23:38 +0100

    Napravljen Appointment (Termin) kontroler. Implementiran CRUD.

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,10 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\AppointmentController;
 
 // Public routes
 Route::get('/services', [ServiceController::class, 'index']);
 Route::get('/service/{id}', [ServiceController::class, 'show']);
+
+// Appointment routes
+Route::get('/appointments', [AppointmentController::class, 'index']); 
+Route::get('/appointment/{id}', [AppointmentController::class, 'show']);
 
 // Authentication routes
 Route::post('/register', [AuthController::class, 'register']);
@@ -22,4 +27,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/service', [ServiceController::class, 'store']); 
     Route::put('/service/{id}', [ServiceController::class, 'update']);
     Route::delete('/service/{id}', [ServiceController::class, 'destroy']);
+
+    Route::post('/appointment', [AppointmentController::class, 'store']);
+    Route::put('/appointment/{id}', [AppointmentController::class, 'update']); 
+    Route::delete('/appointment/{id}', [AppointmentController::class, 'destroy']);
 });
