849e2096871c93d22c59e73aa03e08ebcc29f2b1
Nikola Nestorovic <nestornikola002@gmail.com>
2025-01-01 21:58:11 +0100

    Ubacena Registracija, zajedno sa css-om.

--- react_frontend/src/routes/AppRoutes.js
+++ react_frontend/src/routes/AppRoutes.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage';
+import Register from '../pages/Register/Register';
 
 function AppRoutes() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                {/* Add more routes here */}
+                <Route path="/register" element={<Register />} />
             </Routes>
         </Router>
     );
