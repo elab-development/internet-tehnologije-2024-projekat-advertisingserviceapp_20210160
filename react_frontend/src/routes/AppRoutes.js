03cdfca3a77b598e56b1234627d523b3397c9756
minacaslav <minanikolic2002@gmail.com>
2025-01-02 20:27:49 +0100

    Dodat je header za stranice

--- react_frontend/src/routes/AppRoutes.js
+++ react_frontend/src/routes/AppRoutes.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from '../components/Header/Header'; 
 import HomePage from '../pages/HomePage/HomePage';
 import Register from '../pages/Register/Register';
+import Login from '../pages/Login/Login'; 
 
 function AppRoutes() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Header /> 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default AppRoutes;
