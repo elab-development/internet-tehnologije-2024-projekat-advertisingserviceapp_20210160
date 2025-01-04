10ab5eb82a22666e1c70b78e619afe1c30db861b
Nikola Nestorovic <nestornikola002@gmail.com>
2025-01-04 14:05:29 +0100

    Dodat ProvidersList (js, css). Dodat kontroler za provider-e.

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\TimeSlotController;
+use App\Http\Controllers\ProviderController;
 
 // Authentication routes
 Route::post('/register', [AuthController::class, 'register']);
@@ -23,21 +24,22 @@ Route::get('/appointment/{id}', [AppointmentController::class, 'show']);
 Route::get('/time-slots', [TimeSlotController::class, 'index']); 
 Route::get('/time-slot/{id}', [TimeSlotController::class, 'show']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-    
-    Route::post('/service', [ServiceController::class, 'store']); 
-    Route::put('/service/{id}', [ServiceController::class, 'update']);
-    Route::delete('/service/{id}', [ServiceController::class, 'destroy']);
+Route::get('/providers', [ProviderController::class, 'index']);
+Route::get('/provider/{id}', [ProviderController::class, 'show']);
 
-    Route::post('/appointment', [AppointmentController::class, 'store']);
-    Route::put('/appointment/{id}', [AppointmentController::class, 'update']); 
-    Route::delete('/appointment/{id}', [AppointmentController::class, 'destroy']);
-    
-    Route::post('/time-slot', [TimeSlotController::class, 'store']);
-    Route::put('/time-slot/{id}', [TimeSlotController::class, 'update']);
-    Route::delete('/time-slot/{id}', [TimeSlotController::class, 'destroy']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/user', function (Request $request) {
+    return $request->user();
 });
+
+Route::post('/service', [ServiceController::class, 'store']); 
+Route::put('/service/{id}', [ServiceController::class, 'update']);
+Route::delete('/service/{id}', [ServiceController::class, 'destroy']);
+
+Route::post('/appointment', [AppointmentController::class, 'store']);
+Route::put('/appointment/{id}', [AppointmentController::class, 'update']); 
+Route::delete('/appointment/{id}', [AppointmentController::class, 'destroy']);
+
+Route::post('/time-slot', [TimeSlotController::class, 'store']);
+Route::put('/time-slot/{id}', [TimeSlotController::class, 'update']);
+Route::delete('/time-slot/{id}', [TimeSlotController::class, 'destroy']);
