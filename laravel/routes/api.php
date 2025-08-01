e1a2b6c3d1526343481ad83dca7b917cad5d04e3
minacaslav <minanikolic2002@gmail.com>
2025-08-01 19:31:04 +0200

    Dodata provera dostupnosti

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -7,56 +7,59 @@ use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\TimeSlotController;
 use App\Http\Controllers\ProviderController;
+use App\Http\Controllers\AvailabilityController;
 
-// Authentication routes
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-// Public routes
 Route::get('/services', [ServiceController::class, 'index']);
 Route::get('/service/{id}', [ServiceController::class, 'show']);
 
-// Appointment routes
 Route::get('/appointments', [AppointmentController::class, 'index']); 
 Route::get('/appointment/{id}', [AppointmentController::class, 'show']);
 
-// TimeSlot routes
 Route::get('/time-slots', [TimeSlotController::class, 'index']); 
 Route::get('/time-slot/{id}', [TimeSlotController::class, 'show']);
 
 Route::get('/providers', [ProviderController::class, 'index']);
 Route::get('/provider/{id}', [ProviderController::class, 'show']);
 
-Route::post('/logout', [AuthController::class, 'logout']);
-Route::get('/user', function (Request $request) {
-    return $request->user();
-});
 
+// Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::get('/user/{id}', function ($id) {
-    $user = \App\Models\User::findOrFail($id);
-
-    if ($user->type === 'customer') {
-        $customer = \App\Models\Customer::with(['appointments.service', 'appointments.timeSlot'])->find($id);
-        $user->appointments = $customer ? $customer->appointments : [];
-    } else {
-        $user->appointments = [];
-    }
-
-    return response()->json($user);
-});
-
-
+    Route::get('/user/{id}', function ($id) {
+      $user = \App\Models\User::findOrFail($id);
+  
+      if ($user->type === 'customer') {
+          $customer = \App\Models\Customer::with(['appointments.service', 'appointments.timeSlot'])->find($id);
+          $user->appointments = $customer ? $customer->appointments : [];
+      } else {
+          $user->appointments = [];
+      }
+  
+      return response()->json($user);
+  });
   
 
-Route::post('/service', [ServiceController::class, 'store']); 
-Route::put('/service/{id}', [ServiceController::class, 'update']);
-Route::delete('/service/{id}', [ServiceController::class, 'destroy']);
+    
 
-Route::post('/appointment', [AppointmentController::class, 'store']);
-Route::put('/appointment/{id}', [AppointmentController::class, 'update']); 
-Route::delete('/appointment/{id}', [AppointmentController::class, 'destroy']);
+    Route::post('/service', [ServiceController::class, 'store']); 
+    Route::put('/service/{id}', [ServiceController::class, 'update']);
+    Route::delete('/service/{id}', [ServiceController::class, 'destroy']);
 
-Route::post('/time-slot', [TimeSlotController::class, 'store']);
-Route::put('/time-slot/{id}', [TimeSlotController::class, 'update']);
-Route::delete('/time-slot/{id}', [TimeSlotController::class, 'destroy']);
+
+    Route::post('/appointment', [AppointmentController::class, 'store']);
+    Route::put('/appointment/{id}', [AppointmentController::class, 'update']); 
+    Route::delete('/appointment/{id}', [AppointmentController::class, 'destroy']);
+
+    Route::post('/availability/generate', [AvailabilityController::class, 'generate']);
+
+    Route::post('/time-slot', [TimeSlotController::class, 'store']);
+    Route::put('/time-slot/{id}', [TimeSlotController::class, 'update']);
+    Route::delete('/time-slot/{id}', [TimeSlotController::class, 'destroy']);
+// });
